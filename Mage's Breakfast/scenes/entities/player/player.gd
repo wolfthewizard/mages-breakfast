f035de40aaf7a5dd1edb7b6f6b3c43cf233a2a31
wolfthewizard <matis306@gmail.com>
2024-01-15 23:28:25 +0100

    added player with movement and simple level

---
+++ Mage's Breakfast/scenes/entities/player/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody3D
+
+
+const SPEED = 0.025
+const JUMP_VELOCITY = 0.015
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
+	look_at(global_position + direction)
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
