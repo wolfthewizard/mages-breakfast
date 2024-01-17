8779b9f415f23481da5bfd79919979f53fe52ca8
wolfthewizard <matis306@gmail.com>
2024-01-17 11:37:21 +0100

    added wip knife attack animation

---
+++ Mage's Breakfast/scenes/entities/knife/knife.gd
@@ -0,0 +1,36 @@
+class_name Knife extends Node3D
+
+
+@export var player: CharacterBody3D
+@export var attack_preparation: float = 1
+@export var attack_delay: float = 0.2
+@export var attack_duration: float = 5
+
+var tween: Tween
+
+
+
+func _ready():
+	mow_attack(Vector3(0, 0.1, 0.08), Vector3(0, 0.1, -0.08))
+	#tween = create_tween().set_trans(Tween.TRANS_CUBIC)
+	#tween.tween_property(self, "global_position", Vector3(-.08, global_position.y, global_position.z), 1)
+
+
+func _physics_process(delta):
+	pass
+
+
+func mow_attack(from: Vector3, to: Vector3):
+	var original_position = global_position
+	var angle = from.angle_to(to)
+	tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_parallel(true)
+	tween.tween_property(self, "global_position", from, attack_preparation)
+	tween.tween_property(self, "basis", Basis.looking_at(to, Vector3.UP), attack_preparation)
+	tween.chain().tween_property(self, "", null, attack_delay)
+	tween.chain().tween_property(self, "global_position", to, attack_duration)
+	tween.chain().tween_property(self, "global_position", original_position, attack_preparation)
+
+
+func _on_killbox_body_entered(body):
+	if body == player:
+		player.queue_free()
