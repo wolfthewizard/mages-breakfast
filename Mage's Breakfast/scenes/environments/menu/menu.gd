1a36b77555b965b30026b4894a28fdb42b9bccf8
wolfthewizard <matis306@gmail.com>
2024-01-25 13:24:43 +0100

    added level as background to menu

--- Mage's Breakfast/scenes/environments/menu/menu.gd
+++ Mage's Breakfast/scenes/environments/menu/menu.gd
@@ -1,8 +1,20 @@
 extends Node3D
 
 
+@onready var spring_arm = $SubViewportContainer/SubViewport/CameraPivot/SpringArm3D
+
+const CAMERA_ROTATION_PAN_TIME = 10
+const MAX_CAMERA_ROTATION = TAU / 20
+
+
 func _ready():
 	EventBus.start_game.connect(_on_start_requested)
+	spring_arm.rotation.y = MAX_CAMERA_ROTATION
+	var vec1 = Vector3(spring_arm.rotation.x, -MAX_CAMERA_ROTATION, spring_arm.rotation.z)
+	var vec2 = Vector3(spring_arm.rotation.x, MAX_CAMERA_ROTATION, spring_arm.rotation.z)
+	var tween = create_tween().set_loops().set_trans(Tween.TRANS_SINE)
+	tween.tween_property(spring_arm, "rotation", vec1, CAMERA_ROTATION_PAN_TIME)
+	tween.tween_property(spring_arm, "rotation", vec2, CAMERA_ROTATION_PAN_TIME)
 
 
 func _on_start_requested():
